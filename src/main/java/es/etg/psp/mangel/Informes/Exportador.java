a316ae34215f7fec5c34f58cb34c8ffe6554d959
Compacten <m.angelprietorodriguez@gmail.com>
2025-10-28 17:21:47 +0100

    Rm.mdFinal

--- src/main/java/es/etg/psp/mangel/Informes/Exportador.java
+++ src/main/java/es/etg/psp/mangel/Informes/Exportador.java
@@ -1,7 +1,5 @@
 package es.etg.psp.mangel.Informes;
 
-
-
 public interface Exportador {
-    String exportar(Informe informe);  // No uses Object aquí
+    String exportar(Informe informe); // No uses Object aquí
 }
